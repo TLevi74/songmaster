0f5ef66d756897af4301b55c819e1096f30f394e
TLevi74 <tarlevente10@gmail.com>
2025-02-12 20:20:04 +0100

    adding refresh token

---
+++ netlify/functions/refresh-token.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+
+exports.handler = async function(event, context) {
+    const clientId = process.env.CLIENT_ID;
+    const clientSecret = process.env.CLIENT_SECRET;
+    const refreshToken = process.env.REFRESH_TOKEN;
+
+    const basicAuth = Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
+
+    const tokenUrl = 'https://accounts.spotify.com/api/token';
+    const data = {
+        grant_type: 'refresh_token',
+        refresh_token: refreshToken
+    };
+
+    try {
+        const response = await axios.post(tokenUrl, new URLSearchParams(data), {
+            headers: {
+                'Authorization': `Basic ${basicAuth}`,
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
+
+        const newAccessToken = response.data.access_token;
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ access_token: newAccessToken })
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Error refreshing token' })
+        };
+    }
+};
